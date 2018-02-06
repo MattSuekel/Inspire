513cf0f26c6e598a6e186d747d3fa10504a2bb66
MattSuekel <matthew.suekel@gmail.com>
2018-02-06 13:55:08 -0700

    - Made Login UI Look Nicer

--- app/src/main/java/com/dashtechnologies/rapidchat/LoginActivity.java
+++ app/src/main/java/com/dashtechnologies/rapidchat/LoginActivity.java
@@ -1,79 +1,120 @@
 package com.dashtechnologies.rapidchat;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.TextInputLayout;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+        import android.app.ProgressDialog;
+        import android.content.Intent;
+        import android.support.annotation.NonNull;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.text.TextUtils;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.ProgressBar;
+        import android.widget.TextView;
+        import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
+        import com.google.android.gms.tasks.OnCompleteListener;
+        import com.google.android.gms.tasks.OnSuccessListener;
+        import com.google.android.gms.tasks.Task;
+        import com.google.firebase.auth.AuthResult;
+        import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.auth.FirebaseUser;
+        import com.google.firebase.auth.GetTokenResult;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.firestore.FirebaseFirestore;
+        import com.google.firebase.iid.FirebaseInstanceId;
+
+        import java.util.HashMap;
+        import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Toolbar mToolbar;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mLoginBtn;
+    private Button mRegPageBtn;
 
-    private TextInputLayout mLoginEmail;
-    private TextInputLayout mLoginPassword;
-
-    private Button mLogin_btn;
-
-    private ProgressDialog mLoginProgress;
+    private ProgressBar mProgressBar;
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore mFirestore;
 
-    private DatabaseReference mUserDatabase;
 
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null){
+
+            sendToMain();
+
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
+        mEmailField = (EditText) findViewById(R.id.login_email);
+        mPasswordField = (EditText) findViewById(R.id.login_password);
+        mLoginBtn = (Button) findViewById(R.id.login_btn);
+        mRegPageBtn = (Button) findViewById(R.id.login_register_btn);
+
+        mProgressBar = (ProgressBar) findViewById(R.id.loginProgress);
+
         mAuth = FirebaseAuth.getInstance();
+        mFirestore = FirebaseFirestore.getInstance();
 
-        mToolbar = (Toolbar) findViewById(R.id.login_toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Login");
-
-
-        mLoginProgress = new ProgressDialog(this);
-
-        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
-
-
-        mLoginEmail = (TextInputLayout) findViewById(R.id.login_email);
-        mLoginPassword = (TextInputLayout) findViewById(R.id.login_password);
-        mLogin_btn = (Button) findViewById(R.id.login_btn);
-
-        mLogin_btn.setOnClickListener(new View.OnClickListener() {
+        mRegPageBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                String email = mLoginEmail.getEditText().getText().toString();
-                String password = mLoginPassword.getEditText().getText().toString();
+                Intent regIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(regIntent);
 
-                if(!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)){
+            }
+        });
 
-                    mLoginProgress.setTitle("Logging In");
-                    mLoginProgress.setMessage("Please wait while we check your credentials.");
-                    mLoginProgress.setCanceledOnTouchOutside(false);
-                    mLoginProgress.show();
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-                    loginUser(email, password);
+                String email = mEmailField.getText().toString();
+                String password = mPasswordField.getText().toString();
+
+                if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+
+                    mProgressBar.setVisibility(View.VISIBLE);
+
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+
+                                Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(mainIntent);
+                                finish();
+
+                                String current_user_id = mAuth.getCurrentUser().getUid();
+                                String deviceToken = FirebaseInstanceId.getInstance().getToken();
+
+                            } else {
+
+                                Toast.makeText(LoginActivity.this, "Error : " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                mProgressBar.setVisibility(View.INVISIBLE);
+
+                            }
+
+                        }
+                    });
 
                 }
 
@@ -83,51 +124,11 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    private void sendToMain() {
 
-
-    private void loginUser(String email, String password) {
-
-
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-
-                if(task.isSuccessful()){
-
-                    mLoginProgress.dismiss();
-
-                    String current_user_id = mAuth.getCurrentUser().getUid();
-                    String deviceToken = FirebaseInstanceId.getInstance().getToken();
-
-                    mUserDatabase.child(current_user_id).child("device_token").setValue(deviceToken).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-
-                            Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(mainIntent);
-                            finish();
-
-
-                        }
-                    });
-
-
-
-
-                } else {
-
-                    mLoginProgress.hide();
-
-                    String task_result = task.getException().getMessage().toString();
-
-                    Toast.makeText(LoginActivity.this, "Error : " + task_result, Toast.LENGTH_LONG).show();
-
-                }
-
-            }
-        });
-
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
 
     }
 }
